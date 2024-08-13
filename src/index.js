609000ac818766c3b6bfeac4d9e961f479a26f53
Marina Kholodova <marinaholodova@gmail.com>
2024-08-13 17:13:26 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+	'.-': 'a',
+	'-...': 'b',
+	'-.-.': 'c',
+	'-..': 'd',
+	'.': 'e',
+	'..-.': 'f',
+	'--.': 'g',
+	'....': 'h',
+	'..': 'i',
+	'.---': 'j',
+	'-.-': 'k',
+	'.-..': 'l',
+	'--': 'm',
+	'-.': 'n',
+	'---': 'o',
+	'.--.': 'p',
+	'--.-': 'q',
+	'.-.': 'r',
+	'...': 's',
+	'-': 't',
+	'..-': 'u',
+	'...-': 'v',
+	'.--': 'w',
+	'-..-': 'x',
+	'-.--': 'y',
+	'--..': 'z',
+	'.----': '1',
+	'..---': '2',
+	'...--': '3',
+	'....-': '4',
+	'.....': '5',
+	'-....': '6',
+	'--...': '7',
+	'---..': '8',
+	'----.': '9',
+	'-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+	let result = "";
+
+	for (let i = 0; i < expr.length / 10; i++) {
+		let stringCharMorse = "";
+		let exprChar = expr.slice(i * 10, i * 10 + 10);
+
+		if (exprChar === '**********') {
+			result += ' ';
+			continue;
+		}
+
+		let exprCharInt = parseInt(exprChar);
+
+		while (exprCharInt !== 0) {
+			if (exprCharInt % 100 === 10) {
+				stringCharMorse = '.' + stringCharMorse;
+
+			} else if (exprCharInt % 100 === 11) {
+				stringCharMorse = '-' + stringCharMorse;
+			}
+			exprCharInt = Math.floor(exprCharInt / 100);
+		}
+		result +=  MORSE_TABLE[stringCharMorse];
+	}
+	return result;
 }
 
+
 module.exports = {
-    decode
+	decode
 }
